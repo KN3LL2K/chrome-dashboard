e9e5a2a6d1cf2a00d295d18e93d1eb33395e7a5c
Harry Bellenie <harrybellenie@gmail.com>
2017-03-25 00:36:52 -0400

    Adds Weather icons, temp data. Styles component.

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import request from 'superagent';
 import $ from 'jquery';
+import Loading from './Loading.jsx';
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       longitude: 51.5007,
-      latitude: 0.1246
+      latitude: 0.1246,
+      loading: true,
+      temp: 0,
+      icon: 'clear-day'
     }
   }
 
@@ -30,7 +34,6 @@ class Weather extends React.Component {
     const latitude = position.coords.latitude
     this.setState({longitude: longitude, latitude: latitude})
     this.getForecast(longitude, latitude);
-    console.log(position.coords.latitude, position.coords.longitude); 
   }
 
   getForecast(longitude, latitude) {
@@ -44,22 +47,43 @@ class Weather extends React.Component {
         let icon = res.body.icon;
         let temperature = res.body.temperature;
         if (success === true) {
-          
+          this.setForecast(temperature, icon)
         } else {
-          
+
         }
-        console.log('//////', res.body)
-      })
+      }.bind(this))
+  }
+
+  setForecast(temp, icon) {
+    this.setState({temp: temp, icon: icon, loading: false})
   }
 
   render() {
-
+    let loading = this.state.loading;
+    let temp = this.state.temp; 
+    let icon = this.state.icon;
     return (
       <div id='weather' className='sq-component ltred bottom-second'>
-      <div></div>
-      
+      { loading ? <Loading /> : <Forecast temp={temp} icon={icon} /> }
       </div>
     );
+  }
+}
+
+class Forecast extends React.Component {
+  render() {
+    let temp = Math.round(this.props.temp);
+    let icon = 'assets/' + this.props.icon + '.svg';
+    return (
+    <div id='forecast'>
+      <img className='icon' src={icon} />
+      <div>
+        <span> {temp} </span> 
+        <img className='fahrenheit' src='assets/fahrenheit.svg' />
+      </div>
+      <a href='https://darksky.net/poweredby/'>Powered By Dark Sky</a>
+    </div>
+      )
   }
 }
 
